a16b2ed7193a6f5774ec690629aa81c0efb6fe14
Tearth <tearthdev@gmail.com>
2020-08-20 11:30:18 +0200

    Add queen operator

---
+++ Cosette/Engine/Board/Operators/QueenOperator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Cosette.Engine.Common;
+using Cosette.Engine.Moves;
+
+namespace Cosette.Engine.Board.Operators
+{
+    public static class QueenOperator
+    {
+        public static int GetAvailableMoves(BoardState boardState, Color color, Span<Move> moves, int offset)
+        {
+            var friendlyOccupancy = color == Color.White ? boardState.WhiteOccupancy : boardState.BlackOccupancy;
+            var enemyOccupancy = color == Color.White ? boardState.BlackOccupancy : boardState.WhiteOccupancy;
+            var queens = color == Color.White ? boardState.WhitePieces[(int)Piece.Queen] : boardState.BlackPieces[(int)Piece.Queen];
+
+            while (queens != 0)
+            {
+                var piece = BitOperations.GetLsb(queens);
+                queens = BitOperations.PopLsb(queens);
+
+                var from = BitOperations.BitScan(piece);
+                var availableMoves = QueenMovesGenerator.GetMoves(boardState.Occupancy, from) & ~friendlyOccupancy;
+
+                while (availableMoves != 0)
+                {
+                    var field = BitOperations.GetLsb(availableMoves);
+                    availableMoves = BitOperations.PopLsb(availableMoves);
+                    var fieldIndex = BitOperations.BitScan(field);
+
+                    moves[offset++] = new Move(from, fieldIndex, Piece.Queen, (field & enemyOccupancy) != 0 ? MoveFlags.Kill : MoveFlags.None);
+                }
+            }
+
+            return offset;
+        }
+    }
+}
