92ca0fb87a31ea4a94b1894f60b7892bda3727fd
Tearth <tearthdev@gmail.com>
2020-08-20 15:18:53 +0200

    Fix pawns movement

--- Cosette/Engine/Board/Operators/PawnOperator.cs
+++ Cosette/Engine/Board/Operators/PawnOperator.cs
@@ -10,25 +10,38 @@ namespace Cosette.Engine.Board.Operators
         {
             offset = GetSinglePush(boardState, color, moves, offset);
             offset = GetDoublePush(boardState, color, moves, offset);
-            offset = GetDiagonalAttacks(boardState, color, color == Color.White ? 9 : -7, BoardConstants.AFile, moves, offset);
-            offset = GetDiagonalAttacks(boardState, color, color == Color.White ? 7 : -9, BoardConstants.HFile, moves, offset);
+            offset = GetDiagonalAttacks(boardState, color, color == Color.White ? 9 : 7, BoardConstants.AFile, moves, offset);
+            offset = GetDiagonalAttacks(boardState, color, color == Color.White ? 7 : 9, BoardConstants.HFile, moves, offset);
 
             return offset;
         }
 
         private static int GetSinglePush(BoardState boardState, Color color, Span<Move> moves, int offset)
         {
-            var pushShift = color == Color.White ? 8 : -8;
-            var promotionRank = color == Color.White ? BoardConstants.HRank : BoardConstants.ARank;
-            var pawns = color == Color.White ? boardState.WhitePieces[(int)Piece.Pawn] : boardState.BlackPieces[(int)Piece.Pawn];
-            var singlePush = (pawns << pushShift) & ~boardState.Occupancy;
+            int shift;
+            ulong promotionRank, pawns;
 
-            while (singlePush != 0)
+            if (color == Color.White)
             {
-                var piece = BitOperations.GetLsb(singlePush);
-                singlePush = BitOperations.PopLsb(singlePush);
+                shift = 8;
+                promotionRank = BoardConstants.HRank;
+                pawns = boardState.WhitePieces[(int) Piece.Pawn];
+                pawns = (pawns << 8) & ~boardState.Occupancy;
+            }
+            else
+            {
+                shift = -8;
+                promotionRank = BoardConstants.ARank;
+                pawns = boardState.BlackPieces[(int)Piece.Pawn];
+                pawns = (pawns >> 8) & ~boardState.Occupancy;
+            }
 
-                var from = BitOperations.BitScan(piece >> pushShift);
+            while (pawns != 0)
+            {
+                var piece = BitOperations.GetLsb(pawns);
+                pawns = BitOperations.PopLsb(pawns);
+
+                var from = BitOperations.BitScan(piece) - shift;
                 var to = BitOperations.BitScan(piece);
 
                 if ((piece & promotionRank) != 0)
@@ -49,20 +62,32 @@ namespace Cosette.Engine.Board.Operators
 
         private static int GetDoublePush(BoardState boardState, Color color, Span<Move> moves, int offset)
         {
-            var pushShift = color == Color.White ? 8 : -8;
-            var launchRank = color == Color.White ? BoardConstants.BRank : BoardConstants.GRank;
-            var pawns = color == Color.White ? boardState.WhitePieces[(int)Piece.Pawn] : boardState.BlackPieces[(int)Piece.Pawn];
+            int shift;
+            ulong startRank, pawns;
 
-            var pawnsAtLaunchRank = pawns & launchRank;
-            var singlePush = (pawnsAtLaunchRank << pushShift) & ~boardState.Occupancy;
-            var doublePush = (singlePush << pushShift) & ~boardState.Occupancy;
-
-            while (doublePush != 0)
+            if (color == Color.White)
             {
-                var piece = BitOperations.GetLsb(doublePush);
-                doublePush = BitOperations.PopLsb(doublePush);
+                shift = 16;
+                startRank = BoardConstants.BRank;
+                pawns = boardState.WhitePieces[(int)Piece.Pawn];
+                pawns = ((pawns & startRank) << 8) & ~boardState.Occupancy;
+                pawns = (pawns << 8) & ~boardState.Occupancy;
+            }
+            else
+            {
+                shift = -16;
+                startRank = BoardConstants.GRank;
+                pawns = boardState.BlackPieces[(int)Piece.Pawn];
+                pawns = ((pawns & startRank) >> 8) & ~boardState.Occupancy;
+                pawns = (pawns >> 8) & ~boardState.Occupancy;
+            }
 
-                var from = BitOperations.BitScan(piece >> (2 * pushShift));
+            while (pawns != 0)
+            {
+                var piece = BitOperations.GetLsb(pawns);
+                pawns = BitOperations.PopLsb(pawns);
+
+                var from = BitOperations.BitScan(piece) - shift;
                 var to = BitOperations.BitScan(piece);
 
                 moves[offset++] = new Move(from, to, Piece.Pawn, 0);
@@ -73,25 +98,37 @@ namespace Cosette.Engine.Board.Operators
 
         private static int GetDiagonalAttacks(BoardState boardState, Color color, int shift, ulong prohibitedFile, Span<Move> moves, int offset)
         {
-            var enemyOccupancy = color == Color.White ? boardState.BlackOccupancy : boardState.WhiteOccupancy;
-            var promotionRank = color == Color.White ? BoardConstants.HRank : BoardConstants.ARank;
-            var pawns = color == Color.White ? boardState.WhitePieces[(int)Piece.Pawn] : boardState.BlackPieces[(int)Piece.Pawn];
+            ulong promotionRank, enemyOccupancy, pawns;
 
-            var attacks = ((pawns & ~prohibitedFile) << shift) & enemyOccupancy;
-            while (attacks != 0)
+            if (color == Color.White)
             {
-                var piece = BitOperations.GetLsb(attacks);
-                attacks = BitOperations.PopLsb(attacks);
+                promotionRank = BoardConstants.HRank;
+                enemyOccupancy = boardState.BlackOccupancy;
+                pawns = boardState.WhitePieces[(int)Piece.Pawn];
+                pawns = ((pawns & ~prohibitedFile) << shift) & enemyOccupancy;
+            }
+            else
+            {
+                promotionRank = BoardConstants.ARank;
+                enemyOccupancy = boardState.WhiteOccupancy;
+                pawns = boardState.BlackPieces[(int)Piece.Pawn];
+                pawns = ((pawns & ~prohibitedFile) >> shift) & enemyOccupancy;
+            }
 
-                var from = BitOperations.BitScan(piece >> shift);
+            while (pawns != 0)
+            {
+                var piece = BitOperations.GetLsb(pawns);
+                pawns = BitOperations.PopLsb(pawns);
+
+                var from = BitOperations.BitScan(piece) - shift;
                 var to = BitOperations.BitScan(piece);
 
                 if ((piece & promotionRank) != 0)
                 {
-                    moves[offset++] = new Move(from, to, Piece.Pawn, MoveFlags.Promotion | MoveFlags.KnightPromotion);
-                    moves[offset++] = new Move(from, to, Piece.Pawn, MoveFlags.Promotion | MoveFlags.BishopPromotion);
-                    moves[offset++] = new Move(from, to, Piece.Pawn, MoveFlags.Promotion | MoveFlags.RookPromotion);
-                    moves[offset++] = new Move(from, to, Piece.Pawn, MoveFlags.Promotion | MoveFlags.QueenPromotion);
+                    moves[offset++] = new Move(from, to, Piece.Pawn, MoveFlags.Kill | MoveFlags.Promotion | MoveFlags.KnightPromotion);
+                    moves[offset++] = new Move(from, to, Piece.Pawn, MoveFlags.Kill | MoveFlags.Promotion | MoveFlags.BishopPromotion);
+                    moves[offset++] = new Move(from, to, Piece.Pawn, MoveFlags.Kill | MoveFlags.Promotion | MoveFlags.RookPromotion);
+                    moves[offset++] = new Move(from, to, Piece.Pawn, MoveFlags.Kill | MoveFlags.Promotion | MoveFlags.QueenPromotion);
                 }
                 else
                 {
