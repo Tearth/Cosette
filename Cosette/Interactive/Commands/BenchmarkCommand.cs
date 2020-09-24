ab587c00a2f970b0e2767f0330ae3d14919a3943
Tearth <tearthdev@gmail.com>
2020-09-24 20:09:41 +0200

    Update benchmark depths

--- Cosette/Interactive/Commands/BenchmarkCommand.cs
+++ Cosette/Interactive/Commands/BenchmarkCommand.cs
@@ -37,19 +37,19 @@ namespace Cosette.Interactive.Commands
             var boardState = new BoardState();
             boardState.SetDefaultState();
 
-            Test(boardState, "Opening", 11);
+            Test(boardState, "Opening", 12);
         }
 
         private void TestMidGame()
         {
             var boardState = FenParser.Parse("r2qr1k1/p2n1p2/1pb3pp/2ppN1P1/1R1PpP2/BQP1n1PB/P4N1P/1R4K1 w - - 0 21");
-            Test(boardState, "Midgame", 11);
+            Test(boardState, "Midgame", 12);
         }
 
         private void TestEndGame()
         {
             var boardState = FenParser.Parse("7r/8/2k3P1/1p1p2Kp/1P6/2P5/7r/Q7 w - - 0 1");
-            Test(boardState, "Endgame", 16);
+            Test(boardState, "Endgame", 17);
         }
 
         private void Test(BoardState boardState, string name, int depth)
