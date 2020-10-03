8f0cb6f43d4147b8ca01cc0ad4d688a8d9d65229
Tearth <tearthdev@gmail.com>
2020-10-03 17:02:04 +0200

    Add Arbiter project

---
+++ Cosette.Arbiter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cosette.Arbiter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
