e2bf3604dd79e20a9384860fac2291c1df43728f
Tearth <tearthdev@gmail.com>
2020-10-04 16:04:22 +0200

    Add tournament scheduler

--- Cosette.Arbiter/Settings/SettingsModel.cs
+++ Cosette.Arbiter/Settings/SettingsModel.cs
@@ -5,18 +5,7 @@ namespace Cosette.Arbiter.Settings
 {
     public class SettingsModel
     {
-        [JsonProperty("engine1_name")]
-        public string Engine1Name { get; set; }
-
-        [JsonProperty("engine1_path")]
-        public string Engine1Path { get; set; }
-
-        [JsonProperty("engine2_name")]
-        public string Engine2Name { get; set; }
-
-        [JsonProperty("engine2_path")]
-        public string Engine2Path { get; set; }
-
+        public List<EngineData> Engines { get; set; }
         public List<string> Options { get; set; }
 
         [JsonProperty("milliseconds_per_move")]
