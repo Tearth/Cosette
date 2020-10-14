3f0f7daa45a481993200b40d537c32d7f930f3bb
Tearth <tearthdev@gmail.com>
2020-10-14 18:58:46 +0200

    Adjust time scheduler

--- Cosette/Engine/Ai/Time/TimeScheduler.cs
+++ Cosette/Engine/Ai/Time/TimeScheduler.cs
@@ -6,7 +6,7 @@ namespace Cosette.Engine.Ai.Time
     {
         public static int CalculateTimeForMove(int remainingTime, int moveNumber)
         {
-            return remainingTime / Math.Max(25, 60 - moveNumber);
+            return remainingTime / Math.Max(20, 40 - moveNumber);
         }
     }
 }
