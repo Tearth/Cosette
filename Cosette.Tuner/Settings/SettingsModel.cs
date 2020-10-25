60eeac00d2136c63fe3cc74468cc5a5aa1b1c858
Tearth <tearthdev@gmail.com>
2020-10-25 22:23:56 +0100

    Add settings loader for Tuner project

---
+++ Cosette.Tuner/Settings/SettingsModel.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Cosette.Tuner.Settings
+{
+    public class SettingsModel
+    {
+        [JsonProperty("engine_path")]
+        public int EnginePath { get; set; }
+
+        [JsonProperty("engine_arguments")]
+        public int EngineArguments { get; set; }
+    }
+}
