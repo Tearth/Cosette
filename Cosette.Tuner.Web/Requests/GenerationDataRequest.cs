0e17032009c171df2159c217ddec116b15f4e7e6
Tearth <tearthdev@gmail.com>
2020-11-03 20:23:40 +0100

    Refactoring

--- Cosette.Tuner.Web/Requests/GenerationDataRequest.cs
+++ Cosette.Tuner.Web/Requests/GenerationDataRequest.cs
@@ -5,7 +5,7 @@ namespace Cosette.Tuner.Web.Requests
     public class GenerationDataRequest
     {
         public string TestId { get; set; }
-        public double Time { get; set; }
+        public double ElapsedTime { get; set; }
 
         public int BestFitness { get; set; }
         public List<GeneDataRequest> BestChromosomeGenes { get; set; }
