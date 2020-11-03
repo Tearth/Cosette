162ddcbcc5bdacaf2accf28f726964a26e4e5737
Tearth <tearthdev@gmail.com>
2020-11-03 20:37:57 +0100

    Add database context

--- Cosette.Tuner.Web/Controllers/HomeController.cs
+++ Cosette.Tuner.Web/Controllers/HomeController.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cosette.Tuner.Web.Database;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cosette.Tuner.Web.Controllers
 {
     public class HomeController : Controller
     {
+        public HomeController(DatabaseContext context)
+        {
+
+        }
+
         public IActionResult Index()
         {
             return View();
---
+++ Cosette.Tuner.Web/Database/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Cosette.Tuner.Web.Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cosette.Tuner.Web.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<ChromosomeGeneModel> ChromosomeGenes { get; set; }
+        public DbSet<ChromosomeModel> Chromosomes { get; set; }
+        public DbSet<EngineStatisticsModel> EngineStatistics { get; set; }
+        public DbSet<GenerationGeneModel> GenerationGenes { get; set; }
+        public DbSet<GenerationModel> Generations { get; set; }
+        public DbSet<TestModel> Tests { get; set; }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+    }
+}
