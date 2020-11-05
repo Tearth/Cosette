3587f5824a85e2ae3e3ef46d489656d96d9a4f61
Tearth <tearthdev@gmail.com>
2020-11-05 17:50:06 +0100

    Refactoring of fitness evaluation

--- Cosette.Tuner/Genetics/EvaluationFitness.cs
+++ Cosette.Tuner/Genetics/EvaluationFitness.cs
@@ -14,87 +14,74 @@ namespace Cosette.Tuner.Genetics
     public class EvaluationFitness : IFitness
     {
         private WebService _webService;
-        private EngineOperator _referenceEngineOperator;
-        private EngineOperator _experimentalEngineOperator;
+        private EvaluationParticipant _referenceParticipant;
+        private EvaluationParticipant _experimentalParticipant;
         private PolyglotBook _polyglotBook;
 
         public EvaluationFitness(WebService webService)
         {
             _webService = webService;
-            _referenceEngineOperator = new EngineOperator(SettingsLoader.Data.EnginePath, SettingsLoader.Data.EngineArguments);
-            _experimentalEngineOperator = new EngineOperator(SettingsLoader.Data.EnginePath, SettingsLoader.Data.EngineArguments);
+            _referenceParticipant = new EvaluationParticipant(new EngineOperator(SettingsLoader.Data.EnginePath, SettingsLoader.Data.EngineArguments));
+            _experimentalParticipant = new EvaluationParticipant(new EngineOperator(SettingsLoader.Data.EnginePath, SettingsLoader.Data.EngineArguments));
             _polyglotBook = new PolyglotBook(SettingsLoader.Data.PolyglotOpeningBook);
 
-            _referenceEngineOperator.Init();
-            _experimentalEngineOperator.Init();
+            _referenceParticipant.EngineOperator.Init();
+            _experimentalParticipant.EngineOperator.Init();
         }
 
         public double Evaluate(IChromosome chromosome)
         {
-            var referenceEngineWins = 0;
-            var experimentalEngineWins = 0;
-            var draws = 0;
-
             for (var geneIndex = 0; geneIndex < SettingsLoader.Data.Genes.Count; geneIndex++)
             {
-                _experimentalEngineOperator.SetOption(SettingsLoader.Data.Genes[geneIndex].Name, chromosome.GetGene(geneIndex).ToString());
+                _experimentalParticipant.EngineOperator.SetOption(SettingsLoader.Data.Genes[geneIndex].Name, chromosome.GetGene(geneIndex).ToString());
             }
 
-            _referenceEngineOperator.ApplyOptions();
-            _experimentalEngineOperator.ApplyOptions();
+            _referenceParticipant.EngineOperator.ApplyOptions();
+            _experimentalParticipant.EngineOperator.ApplyOptions();
 
             var stopwatch = Stopwatch.StartNew();
+            var (whitePlayer, blackPlayer) = (_referenceParticipant, _experimentalParticipant);
+            var winsByTime = 0;
+
             for (var gameIndex = 0; gameIndex < SettingsLoader.Data.GamesPerFitnessTest; gameIndex++)
             {
                 try
                 {
                     var gameData = new GameData(_polyglotBook.GetRandomOpening(SettingsLoader.Data.PolyglotMaxMoves));
-
-                    var whitePlayer = DateTime.UtcNow.Ticks % 2 == 0 ? _referenceEngineOperator : _experimentalEngineOperator;
-                    var blackPlayer = whitePlayer == _referenceEngineOperator ? _experimentalEngineOperator : _referenceEngineOperator;
                     var (playerToMove, opponent) = (whitePlayer, blackPlayer);
 
-                    whitePlayer.InitNewGame();
-                    blackPlayer.InitNewGame();
+                    playerToMove.EngineOperator.InitNewGame();
+                    opponent.EngineOperator.InitNewGame();
 
                     while (true)
                     {
-                        var bestMoveData = playerToMove.Go(gameData.HalfMovesDone, gameData.WhiteClock, gameData.BlackClock);
+                        var bestMoveData = playerToMove.EngineOperator.Go(gameData.HalfMovesDone, gameData.WhiteClock, gameData.BlackClock);
                         if (bestMoveData == null)
                         {
-                            draws++;
+                            playerToMove.History.Add(new ArchivedGame(gameData, GameResult.Draw));
+                            opponent.History.Add(new ArchivedGame(gameData, GameResult.Draw));
                             break;
                         }
 
+                        playerToMove.Logs.Add(bestMoveData.LastInfoData);
                         gameData.MakeMove(bestMoveData);
 
                         if (gameData.GameIsDone)
                         {
                             if (gameData.WhiteClock <= 0 || gameData.BlackClock <= 0)
                             {
-                                if (playerToMove == _referenceEngineOperator)
-                                {
-                                    experimentalEngineWins++;
-                                }
-                                else
-                                {
-                                    referenceEngineWins++;
-                                }
+                                winsByTime++;
                             }
-                            else if (gameData.Winner == Color.None)
+                            
+                            if (gameData.Winner == Color.None)
                             {
-                                draws++;
+                                playerToMove.History.Add(new ArchivedGame(gameData, GameResult.Draw));
+                                opponent.History.Add(new ArchivedGame(gameData, GameResult.Draw));
                             }
                             else
                             {
-                                if (playerToMove == _referenceEngineOperator)
-                                {
-                                    referenceEngineWins++;
-                                }
-                                else
-                                {
-                                    experimentalEngineWins++;
-                                }
+                                playerToMove.History.Add(new ArchivedGame(gameData, GameResult.Win));
+                                opponent.History.Add(new ArchivedGame(gameData, GameResult.Loss));
                             }
 
                             break;
@@ -102,18 +89,20 @@ namespace Cosette.Tuner.Genetics
 
                         (playerToMove, opponent) = (opponent, playerToMove);
                     }
+
+                    (whitePlayer, blackPlayer) = (blackPlayer, whitePlayer);
                 }
                 catch
                 {
-                    _referenceEngineOperator.Restart();
-                    _experimentalEngineOperator.Restart();
+                    _referenceParticipant.EngineOperator.Restart();
+                    _experimentalParticipant.EngineOperator.Restart();
                     gameIndex--;
                 }
             }
 
             var runTime = stopwatch.ElapsedMilliseconds;
             var averageTimePerGame = runTime / SettingsLoader.Data.GamesPerFitnessTest;
-            var fitness = experimentalEngineWins - referenceEngineWins + draws / 2;
+            var fitness = _experimentalParticipant.Wins - _referenceParticipant.Wins + _referenceParticipant.Draws / 2;
 
             var genesList = new List<string>();
             for (var geneIndex = 0; geneIndex < SettingsLoader.Data.Genes.Count; geneIndex++)
@@ -124,11 +113,7 @@ namespace Cosette.Tuner.Genetics
                 genesList.Add($"{name}={value}");
             }
 
-            Console.WriteLine($"[{DateTime.Now}] Run done!");
-            Console.WriteLine($" - reference wins: {referenceEngineWins}, experimental wins: {experimentalEngineWins}, draws: {draws}");
-            Console.WriteLine($" - fitness: {fitness}, {string.Join(", ", genesList)}");
-            Console.WriteLine($" - average time per game: {(double)averageTimePerGame / 1000} s");
-
+            Console.WriteLine($"[{DateTime.Now}] Run done! Fitness: {fitness}");
             return fitness;
         }
     }
---
+++ Cosette.Tuner/Genetics/Game/GameResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Cosette.Tuner.Genetics.Game
+{
+    public enum GameResult
+    {
+        Draw,
+        Win,
+        Loss
+    }
+}
