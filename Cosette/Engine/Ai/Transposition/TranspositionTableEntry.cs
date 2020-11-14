a317e5f278a4ac0229434c8249450f88f592fe12
Tearth <tearthdev@gmail.com>
2020-11-14 18:46:27 +0100

    Reduce transposition table entry size

--- Cosette/Engine/Ai/Transposition/TranspositionTableEntry.cs
+++ Cosette/Engine/Ai/Transposition/TranspositionTableEntry.cs
@@ -5,7 +5,7 @@ namespace Cosette.Engine.Ai.Transposition
 {
     public struct TranspositionTableEntry
     {
-        public uint Key { get; set; }
+        public ushort Key { get; set; }
         public byte Depth { get; set; }
         public short Score { get; set; }
         public byte Age { get; set; }
@@ -14,7 +14,7 @@ namespace Cosette.Engine.Ai.Transposition
 
         public TranspositionTableEntry(ulong hash, byte depth, short score, byte age, Move bestMove, TranspositionTableEntryFlags flags)
         {
-            Key = (uint)(hash >> 32);
+            Key = (ushort)(hash >> 48);
             Depth = depth;
             Score = score;
             Age = age;
@@ -24,7 +24,7 @@ namespace Cosette.Engine.Ai.Transposition
 
         public bool IsKeyValid(ulong hash)
         {
-            return Key == (uint)(hash >> 32);
+            return Key == (ushort)(hash >> 48);
         }
     }
 }
