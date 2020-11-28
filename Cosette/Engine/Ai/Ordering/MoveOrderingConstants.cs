733e3093ca719fd998f58994fb70b4b0621d44a8
Tearth <tearthdev@gmail.com>
2020-11-28 17:10:17 +0100

    Adjust move ordering constants

--- Cosette/Engine/Ai/Ordering/MoveOrderingConstants.cs
+++ Cosette/Engine/Ai/Ordering/MoveOrderingConstants.cs
@@ -5,12 +5,12 @@
         public static short HashMove = 10000;
         public static short Promotion = 5000;
         public static short Castling = 1000;
-        public static short PawnNearPromotion = 150;
-        public static short Capture = 100;
+        public static short PawnNearPromotion = 300;
+        public static short Capture = 200;
         public static short EnPassant = 100;
         public static short KillerMove = 90;
-        public static uint HistoryHeuristicMaxScore = 80;
 
+        public static uint HistoryHeuristicMaxScore = 80;
         public static int KillerSlots = 3;
     }
 }
