a41bd3882c78c8d2757512f30c4ca8776b274845
Tearth <tearthdev@gmail.com>
2020-12-20 14:19:18 +0100

    Adjust search parameters

--- Cosette/Engine/Ai/Search/SearchConstants.cs
+++ Cosette/Engine/Ai/Search/SearchConstants.cs
@@ -11,12 +11,12 @@
         public static int IIDMinimalDepth = 4;
         public static int IIDDepthReduction = 2;
 
-        public static int NullWindowMinimalDepth = 5;
+        public static int NullWindowMinimalDepth = 3;
         public static int NullWindowDepthReduction = 3;
 
         public static int LMRMinimalDepth = 2;
         public static int LMRMovesWithoutReduction = 3;
-        public static int LMRPvNodeDepthReduction = 1;
+        public static int LMRPvNodeDepthReduction = 2;
         public static int LMRNonPvNodeDepthDivisor = 3;
     }
 }
