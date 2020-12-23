81a51a2d19425f2df628f94c8e85ad38ee50b82f
Tearth <tearthdev@gmail.com>
2020-12-23 16:17:06 +0100

    Add Cosette.Tuner.Texel project

---
+++ Cosette.Tuner.Texel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cosette.Tuner.Texel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
