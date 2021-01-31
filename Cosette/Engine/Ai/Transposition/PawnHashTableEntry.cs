214972c8b938ec5705ccc12966324074ea912b1d
Tearth <tearthdev@gmail.com>
2021-01-31 12:35:05 +0100

    Reduce size of pawn hash table entry

--- Cosette/Engine/Ai/Transposition/PawnHashTableEntry.cs
+++ Cosette/Engine/Ai/Transposition/PawnHashTableEntry.cs
@@ -1,23 +1,39 @@
-﻿using System.Runtime.InteropServices;
-
-namespace Cosette.Engine.Ai.Transposition
+﻿namespace Cosette.Engine.Ai.Transposition
 {
     public struct PawnHashTableEntry
     {
-        public ushort Key { get; set; }
-        public short OpeningScore { get; set; }
-        public short EndingScore { get; set; }
+        public ushort Key => (ushort)(_data >> 20);
+        public short EndingScore
+        {
+            get
+            {
+                var result = (short)((_data >> 10) & 0x3FF);
+                return result < 512 ? result : (short)(result - 1024);
+            }
+        }
+        public short OpeningScore
+        {
+            get
+            {
+                var result = (short)(_data & 0x3FF);
+                return result < 512 ? result : (short)(result - 1024);
+            }
+        }
+
+        private uint _data;
 
         public PawnHashTableEntry(ulong hash, short openingScore, short endingScore)
         {
-            Key = (ushort)(hash >> 48);
-            OpeningScore = openingScore;
-            EndingScore = endingScore;
+            var hashPart = (uint)((hash >> 52) << 20);
+            var endingScorePart = (uint)((endingScore & 0x3FF) << 10);
+            var openingScorePart = (uint)(openingScore & 0x3FF);
+
+            _data = hashPart | endingScorePart | openingScorePart;
         }
 
         public bool IsKeyValid(ulong hash)
         {
-            return Key == hash >> 48;
+            return Key == hash >> 52;
         }
     }
 }
